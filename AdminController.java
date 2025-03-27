b4ab5f1aa1b278cb7a57e60d5bb6d724502f79c5
herta099 <164089646+herta099@users.noreply.github.com>
2025-03-27 15:35:50 +0100

    Admin Controller

---
+++ AdminController.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author h
+ */
+public class AdminController  {
+    public String getDashboardData() {
+         return "Menaxho projektet\nShto/Fshi punonjës\nMenaxho pagat";
+     }
+     
+ }
+ 
+
